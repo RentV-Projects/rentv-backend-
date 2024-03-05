c0752c7cf884cd6e0689d252a64ef6e680fb5784
Wyeben <yila.ben@yahoo.com>
2024-03-05 09:00:43 +0100

    added code

--- src/main/java/com/example/rentv/RentVApplication.java
+++ src/main/java/com/example/rentv/RentVApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentVApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentVApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RentVApplication.class, args);
+    }
 
 }
+
+
+
