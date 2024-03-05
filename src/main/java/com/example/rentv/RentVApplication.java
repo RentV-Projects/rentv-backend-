d0d917288d2fc30c790e823acbfd61732021d024
Wyeben <yila.ben@yahoo.com>
2024-03-05 08:55:03 +0100

    first commit

---
+++ src/main/java/com/example/rentv/RentVApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentv/RentVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
